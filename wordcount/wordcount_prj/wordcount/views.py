24ca4f3ba0e66ddb5123ed78e22c181efc0f7c67
Yebin Kim <dpqls161@gmail.com>
2026-04-28 11:08:07 +0900

    [0428] wordCount 실

---
+++ wordcount/wordcount_prj/wordcount/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def word_count(request):
+    return render(request, "word_count.html")
+
+def result(request):
+    return render(request, "result.html")
+
+#백엔드
+def result(request):
+    entered_text = request.GET['fulltext']
+    word_list = entered_text.split()
+    countwords = 0
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+        countwords += 1
+
+    return render(request, "result.html", {'alltext' : entered_text, 'dictionary' : word_dictionary.items(), 'wordstotalcount' : countwords})
+#작은따옴표 -> 백엔드 프론트엔드 연동 변수
